ddfc6568b5d10ce27fe4bdd86df45050ef84b2a4
Talhahafeezme <talhahafeezme@gmail.com>
2022-06-16 18:43:14 +0500

    Create custom.js

---
+++ custom.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function() {
+jQuery('.quote-slide').slick({
+dots: false,
+infinite: true,  
+speed: 300,
+slidesToShow: 1,
+adaptiveHeight: true,
+autoplay: true,
+autoplaySpeed: 5000,
+focusOnSelect: true,
+pauseOnHover:true
+});
